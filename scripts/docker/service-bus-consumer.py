5795029ff884eb979531314ff1f9a74fb8c3c670
Alastair Lock <alastair.lock@gmail.com>
2025-05-08 12:01:21 +0100

    DTOSS-8695: Add in try catch when connecting to service bus

--- scripts/docker/service-bus-consumer.py
+++ scripts/docker/service-bus-consumer.py
@@ -2,6 +2,7 @@ import os
 import time
 from dotenv import load_dotenv
 from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from azure.servicebus.exceptions import ServiceBusError
 
 load_dotenv()
 
@@ -11,16 +12,26 @@ if not connection_str:
 
 client = ServiceBusClient.from_connection_string(connection_str)
 
-with client:
-    receiver = client.get_queue_receiver(queue_name="queue.1", max_wait_time=5)
-    with receiver:
-        print("Listening for messages...")
-        while True:
-            messages = receiver.receive_messages(max_message_count=10)
-            if not messages:
-                time.sleep(1)
-                continue
+try:
+    with client:
+        receiver = client.get_queue_receiver(queue_name="queue.1", max_wait_time=5)
+        with receiver:
+            print("Listening for messages...")
+            while True:
+                try:
+                    messages = receiver.receive_messages(max_message_count=10)
+                    if not messages:
+                        time.sleep(1)
+                        continue
 
-            for msg in messages:
-                print("Received:", str(msg))
-                receiver.complete_message(msg)
+                    for msg in messages:
+                        print("Received:", str(msg))
+                        receiver.complete_message(msg)
+
+                except ServiceBusError as e:
+                    print(f"ServiceBusError occurred while receiving or completing messages: {e}")
+                    time.sleep(5)  # backoff before retrying
+
+except ServiceBusError as e:
+    print(f"ServiceBusError occurred when connecting to Service Bus: {e}")
+
--- scripts/docker/service-bus-producer.py
+++ scripts/docker/service-bus-producer.py
@@ -9,9 +9,18 @@ if not connection_str:
     raise ValueError("Missing SERVICE_BUS_CONNECTION_STR in .env file")
 client = ServiceBusClient.from_connection_string(connection_str)
 
-with client:
-    sender = client.get_queue_sender(queue_name="queue.1")
-    with sender:
-        message = ServiceBusMessage("Hello from local sender!")
-        sender.send_messages(message)
-        print("Message sent.")
+try:
+    with client:
+        sender = client.get_queue_sender(queue_name="queue.1")
+        with sender:
+            try:
+                message = ServiceBusMessage("Hello from local sender!")
+                sender.send_messages(message)
+                print("Message sent.")
+
+            except ServiceBusError as e:
+                print(f"ServiceBusError occurred while produce a message: {e}")
+
+
+except ServiceBusError as e:
+    print(f"ServiceBusError occurred when connecting to Service Bus: {e}")
