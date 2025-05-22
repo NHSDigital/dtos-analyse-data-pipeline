7caa8fb51e5c26c090a09899dcdaac993cacb538
Alastair Lock <alastair.lock@gmail.com>
2025-05-22 12:27:40 +0100

    DTOSS-9002: Deploy service bus into the hub vnet - DEV

--- infrastructure/environments/cloud/tf-core/function_app.tf
+++ infrastructure/environments/cloud/tf-core/function_app.tf
@@ -86,7 +86,7 @@ locals {
       for function_key, function_values in local.service_bus_function_app_map : merge({
         service_bus_key   = service_bus_key    # 1st iterator
         function_key      = function_key # 2nd iterator
-        service_bus_value = event_value
+        service_bus_value = service_bus_value
       }, function_values) # the block of key/value pairs for a specific collection
       if contains(keys(function_values), "service_bus_topic_producers")
       && (function_values.service_bus_topic_producers != null ? length(function_values.service_bus_topic_producers) > 0 : false)
