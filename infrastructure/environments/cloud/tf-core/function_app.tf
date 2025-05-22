5b63595d96889a86215f02afe5212377faa9cc1f
Alastair Lock <alastair.lock@gmail.com>
2025-05-22 12:08:08 +0100

    DTOSS-9002: Deploy service bus into the hub vnet - DEV

--- infrastructure/environments/cloud/tf-core/function_app.tf
+++ infrastructure/environments/cloud/tf-core/function_app.tf
@@ -100,15 +100,16 @@ locals {
 }
 
 # Use the merged map in your resources
-# resource "azurerm_role_assignment" "function_send_to_topic" {
-#   for_each = local.unified_service_bus_object_map
+resource "azurerm_role_assignment" "function_send_to_topic" {
+  for_each = local.unified_service_bus_object_map
 
-#   principal_id         = module.functionapp["${each.value.function_key}-${each.value.event_value.region}"].function_app_sami_id
-#   role_definition_name = "Azure Service Bus Data Sender"
-#   # scope                = data.terraform_remote_state.hub.outputs.service_bus_topic["${each.value.event_key}"].id
-#   scope                = module.azure_service_bus[each.value.event_key].servicebus_topic_ids[each.value.event_key]
-# }
-
+  principal_id         = module.functionapp["${each.value.function_key}-${each.value.event_value.region}"].function_app_sami_id
+  role_definition_name = "Azure Service Bus Data Sender"
+  # scope                = module.event_grid_topic["${each.value.event_key}"].id
+  # scope                = data.terraform_remote_state.hub.outputs.service_bus_topic["${each.value.event_key}"].id
+  scope                = module.azure_service_bus["${each.value.event_key}"].id
+}
+# "${object.service_bus_key}-${object.region}"
 
 /* --------------------------------------------------------------------------------------------------
   Local variables used to create the Environment Variables for the Function Apps
